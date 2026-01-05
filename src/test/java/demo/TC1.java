86bd4eb2e3979166b3edb0d3cfff8a1e65ae4666
Bhagya54 <testingtrainer54@gmail.com>
2026-01-05 19:43:51 +0530

    Git Y - IST

--- src/test/java/demo/TC1.java
+++ src/test/java/demo/TC1.java
@@ -3,5 +3,6 @@ package demo;
 public class TC1 {
     public static void main(String[] args) {
         System.out.println("Person - EST - GitX");
+        System.out.println("Person - IST - GitY");
     }
 }
